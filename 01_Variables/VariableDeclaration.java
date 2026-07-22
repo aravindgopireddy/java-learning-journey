f480d03b6b79a7019f0e06337432bfc40649dc21
Aravind Reddy <aravindgopireddy02@gmail.com>
2026-07-22 19:27:46 +0530

    added variables examples

---
+++ 01_Variables/VariableDeclaration.java
@@ -0,0 +1,18 @@
+public class VariableDeclaration {
+    public static void main(String[] args) {
+
+        int age;
+        double salary;
+        char grade;
+        String name;
+
+        age = 20;
+        salary = 25000.50;
+        grade = 'A';
+        name = "Aravind";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+}}
