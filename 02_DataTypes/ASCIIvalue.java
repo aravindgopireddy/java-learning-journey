2732f20dbe5d1f60bccc658773954de1456b31b1
Aravind Reddy <aravindgopireddy02@gmail.com>
2026-07-22 22:23:11 +0530

    add ASCIIvalue example to demonstrate character to ASCII conversion

---
+++ 02_DataTypes/ASCIIvalue.java
@@ -0,0 +1,9 @@
+public class ASCIIvalue {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int ascii = ch;
+
+        System.out.println("Character: " + ch);
+        System.out.println("ASCII Value: " + ascii);
+    }
+}
