8aa477b30e13e717e008c50434ba0627fb180764
Aravind Reddy <aravindgopireddy02@gmail.com>
2026-07-22 19:34:45 +0530

    added variable initialization program

---
+++ 01_Variables/VariableInitialization.java
@@ -0,0 +1,16 @@
+public class VariableInitialization {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double salary = 25000.50;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Aravind";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
