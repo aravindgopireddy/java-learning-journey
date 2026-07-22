d19ada3e5c5ddce2926249202d233859a2932bb6
Aravind Reddy <aravindgopireddy02@gmail.com>
2026-07-22 22:17:40 +0530

    implicit conversion

---
+++ 02_DataTypes/ImplicitConversion.java
@@ -0,0 +1,9 @@
+public class ImplicitConversion{
+
+    public static void main(String[] args){
+        int num = 100;
+        double value = num; 
+        
+        System.out.println("Integer value: " + num);
+        System.out.println("Double value: " + value);
+}}
