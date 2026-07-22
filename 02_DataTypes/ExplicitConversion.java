1a7e04cb3303af149c67557d2531bab0ed146f2e
Aravind Reddy <aravindgopireddy02@gmail.com>
2026-07-22 22:20:27 +0530

    add ExplicitConversion example to demonstrate type casting

---
+++ 02_DataTypes/ExplicitConversion.java
@@ -0,0 +1,9 @@
+public class ExplicitConversion {
+    public static void main(String[] args) {
+        double num = 99.99;
+        int value = (int) num;
+
+        System.out.println("Double value: " + num);
+        System.out.println("Integer value: " + value);
+    }
+}
