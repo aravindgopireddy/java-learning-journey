51966ef00c1c6cd27d4e920551290411b4da646e
Aravind Reddy <aravindgopireddy02@gmail.com>
2026-07-22 20:56:11 +0530

    added all primitive datatypes

---
+++ 01_Variables/DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+    public static void main(String[] args){
+
+        byte b = 123;
+        short s = 12345;
+        int i = 123456;
+        long l = 12345678910L;
+        float f = 12.34f;
+        double d = 12345.6789;
+        char c = 'A';
+        boolean B = true;
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + B);
+
+    }
+}
