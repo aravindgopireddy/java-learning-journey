37234b992d9895873975265c2fadbc016b9134b6
Aravind Reddy <aravindgopireddy02@gmail.com>
2026-07-22 22:44:06 +0530

    addition of two integers by scanner

---
+++ 03_InputOutput/Add_two_nums.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Add_two_nums{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+     
+        System.out.println("Enter first number  : ");
+        int a=sc.nextInt();
+
+        System.out.println("Enter secomd number : ");
+        int b=sc.nextInt();
+
+        System.out.println("Result : "+(a+b));
+sc.close();
+    }
+}
