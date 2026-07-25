aeb6c1aa43ec834435c92ce030235c2d55f045bd
Aravind Reddy <aravindgopireddy02@gmail.com>
2026-07-25 21:55:38 +0530

    added OddOrEven to determine if a number is odd or even

---
+++ 04_Operators/OddOrEven.java
@@ -0,0 +1,9 @@
+public class OddOrEven {
+    public static void main(String[] args){
+
+     int a=3;
+
+      String result = ((a%2)==0) ? "It is a Even":"It is a odd";
+        System.out.println(result);
+    }
+}
