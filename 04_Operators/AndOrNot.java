4f770074852ff53d8e12c374ff5181c438190f5c
Aravind Reddy <aravindgopireddy02@gmail.com>
2026-07-25 21:46:51 +0530

    added AndOrNot to demonstrate logical operators in Java

---
+++ 04_Operators/AndOrNot.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class AndOrNot{
+public static void main(String[] args){
+
+int a = 20;
+int b = 100;
+
+System.out.println(a >= 20  &&  b <101);
+
+System.out.println(a >= 20  ||  b <99);
+
+System.out.println(!(b<=100));
+}    
+}
