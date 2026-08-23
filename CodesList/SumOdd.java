64e02b12da2ffbc8a458454ac9f58879e515e168
Aravind Reddy <aravindgopireddy02@gmail.com>
2026-08-23 12:10:59 +0530

    added SumOdd to calculate the sum of all odd numbers from 1 to 100

---
+++ CodesList/SumOdd.java
@@ -0,0 +1,11 @@
+package CodesList;
+
+public class SumOdd {
+    public static void main(String[] args){
+        int sum=0;
+        for(int i=1;i<100;i+=2){
+            sum=sum+i;
+        }
+        System.out.println("Sum :"+sum);
+    }
+}
